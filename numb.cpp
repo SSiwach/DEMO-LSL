bdbbf492cecfd837e23f42589ce219336982295a
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-02-04 18:23:27 +0530

    Add files via upload

---
+++ numb.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+
+{
+	cout<< 40<<endl;
+	cout<<5-7<<endl;
+	cout<<5*7<<endl;
+	cout<<10%3<<endl;
+	cout<<4+5*10<<endl;
+	int wnum =5;
+	double dnum = 5.5;
+	 wnum += 80;
+	cout<<wnum<<endl;
+	cout<< pow(5,2)<<" Square of 5"<<endl;
+	cout<<sqrt(36)<<" Square root of 36"<<endl;
+	cout<<round(4.6)<<" Round of 4.6"<<endl;
+	cout<<ceil(4.5)<<" Round of of 4.5"<<endl;
+	cout<<floor(4.8)<<" Floor of 4.8"<<endl;
+	cout<<fmax(3,22)<<" Maxmium of 3 and 22"<<endl;
+	
+return 0;
+}
